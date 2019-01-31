0b2c04958df3e5bbdcc5ca4c1f8d78a5bc0ecf4f
Jared Wiegand <jaredwiegand@Jareds-MacBook-Pro-2.local>
2019-01-31 13:54:26 -0600

    Added comments to incident_complaint_table_code.R

--- r/incident_complaint_table_code.R
+++ r/incident_complaint_table_code.R
@@ -1,6 +1,18 @@
+# =============================================================================
+# Creating incident complaint table
+# Created: 2019-01-28
+# Updated: 2019-01-31
+# =============================================================================
+
+# Here is where I created a path to the data..
 path <- file.path("Documents", "Research Projects", "DETECT Research", "DETECT Data", "symptoms.feather")
+
 library("feather", lib.loc="/Library/Frameworks/R.framework/Versions/3.5/Resources/library")
-feather(path)
+
+feather(path) # loading feather package
+
 Incident <- read_feather(path)
+
 View(Incident)
+
 table(incident_complaint)
